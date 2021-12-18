9871f00ad45ffb45a657ca3bfd2ca549b6a12535
Koryun Demirkhanyan <koryundemirkhanyan@gmail.com>
2021-12-18 20:09:41 +0400

    new

---
+++ data_types/arithmeticCopy.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main () {
+	double a;
+	int b;
+	std::cout << "Enter your numbers \n";
+	std::cin >> a >> b;
+	std::cout << a*b << "\n" << a/b << "\n" << a+b << "\n" << a-b << "\n";
+    return 0;
+}
