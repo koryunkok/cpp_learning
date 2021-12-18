e325f42fb19b17aaec5018c321fd1f8d05d7d9fc
Koryun Demirkhanyan <koryundemirkhanyan@gmail.com>
2021-12-18 20:05:43 +0400

    delete

--- data_types/arithmetic.cpp
+++ data_types/arithmetic.cpp
@@ -1,10 +1,1 @@
-#include <iostream>
-
-int main () {
-	double a;
-	int b;
-	std::cout << "Enter your numbers \n";
-	std::cin >> a >> b;
-	std::cout << a*b << "\n" << a/b << "\n" << a+b << "\n" << a-b << "\n";
-    return 0;
-}
+test rm file
