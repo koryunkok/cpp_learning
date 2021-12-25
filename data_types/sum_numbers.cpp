5fffb679a1a9cd05b56264d8e6b678ad51ed6078
Koryun Demirkhanyan <koryundemirkhanyan@gmail.com>
2021-12-25 14:26:20 +0400

    sum

--- data_types/sum_numbers.cpp
+++ data_types/sum_numbers.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
-int sum_of_numbers (int number) {
-	int a,b;
+int sum_of_numbers (int a, int b) {
+	
 	int sum;
 	sum = a + b;
 	return sum;
 }
  int main () {
-	 int number;
-	 std::cout << "Enter number = ";
-	 std::cin >> number;
-	 std::cout << "Sum of the numbers : " << sum_of_numbers (number) << std::endl;
+	 int x, y;
+	 std::cout << "Enter number x = ";
+	 std::cin >> x;
+         std::cout << "Enter number y = ";
+	 std::cin >> y;
+	 std::cout << "Sum of the numbers : " << sum_of_numbers (x , y) << std::endl;
 
 	 return 0;
  }
